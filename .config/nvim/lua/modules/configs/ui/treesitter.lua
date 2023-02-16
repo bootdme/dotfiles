a4ff269fa4ef327e5af87fe971ab5aae9cbaac9f
David Estephanian <desteph@protonmail.com>
2023-02-16 12:10:41 -0800

    Added links & removed disabled feature for TS

--- .config/nvim/lua/modules/configs/ui/treesitter.lua
+++ .config/nvim/lua/modules/configs/ui/treesitter.lua
@@ -1,3 +1,4 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter
 return function()
     require("nvim-treesitter.configs").setup({
         ensure_installed = {
@@ -22,16 +23,6 @@ return function()
 
         highlight = {
             enable = true,
-
-            -- Disable treesitter highlight for large files
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
         },
 
         -- Disable tressitter indentation
