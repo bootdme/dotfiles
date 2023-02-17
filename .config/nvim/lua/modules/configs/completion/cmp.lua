424e20549b6cb694e52539fadcf8ecbc27e62738
David Estephanian <desteph@protonmail.com>
2023-02-16 23:55:39 -0800

    Added luasnip & added tab to cmp

--- .config/nvim/lua/modules/configs/completion/cmp.lua
+++ .config/nvim/lua/modules/configs/completion/cmp.lua
@@ -2,6 +2,10 @@
 return function()
 	local compare = require("cmp.config.compare")
 
+    local t = function(str)
+        return vim.api.nvim_replace_termcodes(str, true, true, true)
+    end
+
 	compare.lsp_scores = function(entry1, entry2)
 		local diff
 		if entry1.completion_item.score and entry2.completion_item.score then
@@ -39,6 +43,24 @@ return function()
 				i = cmp.mapping.abort(),
 				c = cmp.mapping.close(),
 			}),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif require("luasnip").expand_or_jumpable() then
+					vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif require("luasnip").jumpable(-1) then
+					vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 		}),
 		snippet = {
 			expand = function(args)
--- .config/nvim/lua/modules/configs/completion/luasnip.lua
+++ .config/nvim/lua/modules/configs/completion/luasnip.lua
@@ -1,3 +1,10 @@
+-- https://github.com/L3MON4D3/LuaSnip 
 return function()
-
+	require("luasnip").config.set_config({
+		history = true,
+		updateevents = "TextChanged,TextChangedI",
+		delete_check_events = "TextChanged,InsertLeave",
+	})
+	require("luasnip.loaders.from_lua").lazy_load()
+	require("luasnip.loaders.from_vscode").lazy_load()
 end
