8451f8403822d024f3afda7d95de09369f8e4306
David Estephanian <desteph@protonmail.com>
2023-02-19 10:29:33 -0800

    Added func for sets & removed folding

---
+++ .config/nvim/lua/core/func.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.set(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	options = vim.tbl_deep_extend("force", options, opts or {})
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+return M
--- .config/nvim/lua/core/sets.lua
+++ .config/nvim/lua/core/sets.lua
@@ -1,43 +1,39 @@
 -- Variables
-local set = vim.keymap.set
-local opts = { noremap = true, silent = true, nowait = true }
+local set = require("core.func").set
 
 -- Move to beginning/end of line
-set("n", "H", "^", opts)
-set("n", "L", "$", opts)
+set("n", "H", "^")
+set("n", "L", "$")
 
 -- Move blocks of code up/down
 set("v", "J", ":m '>+1<CR>gv=gv")
 set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Enable folding
-set("n", "F", "za", opts)
-
 -- Smarter indenting
-set("v", ">", ">gv|", opts)
-set("v", "<", "<gv", opts)
+set("v", ">", ">gv|")
+set("v", "<", "<gv")
 
 -- Select all
-set("n", "<C-a>", "gg<S-v>G", opts)
+set("n", "<C-a>", "gg<S-v>G")
 
 -- Keep cursor in-place if below line append to current line when moving
 set("n", "J", "mzJ`z")
 
 -- Search and highlight
-set("n", "n", "nzzzv", opts)
-set("n", "N", "Nzzzv", opts)
-set("n", "*", "*zzzv", opts)
-set("n", "#", "#zzzv", opts)
-set("n", "<esc>", "<cmd>noh<cr>", opts)
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+set("n", "*", "*zzzv")
+set("n", "#", "#zzzv")
+set("n", "<esc>", "<cmd>noh<cr>")
 
 -- Never press Q
 set("n", "Q", "<nop>")
 
 -- Make Ascii art font
-set("n", "<leader>aA", "<cmd>.!toilet -w 206 -f standard<cr>", opts)
-set("n", "<leader>aa", "<cmd>.!toilet -w 206 -f small<cr>", opts)
+set("n", "<leader>aA", "<cmd>.!toilet -w 206 -f standard<cr>")
+set("n", "<leader>aa", "<cmd>.!toilet -w 206 -f small<cr>")
 -- Make Ascii border
-set("n", "<leader>ab", "<cmd>.!toilet -f term -F border<cr>", opts)
+set("n", "<leader>ab", "<cmd>.!toilet -f term -F border<cr>")
 -- ┌────┐
 -- │test│
 -- └────┘
