186166e21c87f67dcc11737fbdc447c3257bbe05
David Estephanian <desteph@protonmail.com>
2023-02-21 00:33:09 -0800

    Added comment config

---
+++ .config/nvim/lua/modules/plugins/editor.lua
@@ -0,0 +1,10 @@
+local editor = {}
+
+editor["numToStr/Comment.nvim"] = {
+	lazy = true,
+	event = { "BufNewFile", "BufReadPre" },
+	config = require("editor.comment"),
+	dependencies = "nvim-treesitter/nvim-treesitter",
+}
+
+return editor
