cc7e0949e4f4a8dd0e8d526bf2f673b79f4630fe
David Estephanian <desteph@protonmail.com>
2023-02-21 23:41:02 -0800

    Removed some telescope plugins

--- .config/nvim/lua/modules/configs/tool/telescope.lua
+++ .config/nvim/lua/modules/configs/tool/telescope.lua
@@ -16,12 +16,6 @@ return function()
 				"docs/",
 			},
 		},
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 		extensions = {
 			-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
 			fzf = {
@@ -29,12 +23,6 @@ return function()
 				override_generic_sorter = true,
 				override_file_sorter = true,
 				case_mode = "smart_case",
-			},
-			-- https://github.com/nvim-telescope/telescope-frecency.nvim
-			frecency = {
-				show_scores = true,
-				show_unindexed = true,
-				ignore_patterns = { "*.git/*", "*/tmp/*" },
 			},
 			-- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
 			live_grep_args = {
@@ -50,8 +38,6 @@ return function()
 	})
 
 	-- Load telescope plugins
-	require("telescope").load_extension("env")
 	require("telescope").load_extension("fzf")
-	require("telescope").load_extension("frecency")
 	require("telescope").load_extension("live_grep_args")
 end
--- .config/nvim/lua/modules/plugins/tool.lua
+++ .config/nvim/lua/modules/plugins/tool.lua
@@ -13,15 +13,8 @@ tool["nvim-telescope/telescope.nvim"] = {
 	config = require("tool.telescope"),
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		{ "LinArcX/telescope-env.nvim" },
 		{ "nvim-telescope/telescope-live-grep-args.nvim" },
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		{
-			"nvim-telescope/telescope-frecency.nvim",
-			dependencies = {
-				{ "kkharji/sqlite.lua" },
-			},
-		},
 		{ "nvim-tree/nvim-web-devicons" },
 	},
 }
