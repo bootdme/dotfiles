10978be6d89b1368359532c7143466b464008ed1
David Estephanian <desteph@protonmail.com>
2023-02-22 09:46:52 -0800

    Added/Deleted configs from gitsigns and i-b

--- .config/nvim/lua/modules/configs/ui/gitsigns.lua
+++ .config/nvim/lua/modules/configs/ui/gitsigns.lua
@@ -2,30 +2,10 @@
 return function()
 	require("gitsigns").setup({
 		signs = {
-			add = {
-				hl = "GitSignsAdd",
-				text = "┃",
-				numhl = "GitSignsAddNr",
-				linehl = "GitSignsAddLn",
-			},
-			change = {
-				hl = "GitSignsChange",
-				text = "┃",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-			delete = {
-				hl = "GitSignsDelete",
-				text = "▁",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "▔",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
+			add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+			change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+			delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+			topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
 			changedelete = {
 				hl = "GitSignsChange",
 				text = "~",
@@ -54,12 +34,7 @@ return function()
             map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
             map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
 		end,
-		watch_gitdir = { interval = 1000, follow_files = true },
-		current_line_blame = false,
-		current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
-		sign_priority = 6,
-		update_debounce = 100,
-		status_formatter = nil, -- Use default
+		update_debounce = 200,
 		word_diff = false,
 		diff_opts = { internal = true },
 	})
