70cc7d9218c2283122803366253e99afbdfcdbb8
David Estephanian <desteph@protonmail.com>
2023-03-08 13:49:29 -0800

    Updated plugins

--- .config/nvim/lua/modules/configs/completion/lsp/lua_ls.lua
+++ .config/nvim/lua/modules/configs/completion/lsp/lua_ls.lua
@@ -8,24 +8,17 @@ table.insert(runtime_path, 'lua/?/init.lua')
 local settings = {
     Lua = {
         runtime = {
+            diagnostics = {
+                enable = true,
+                globals = { 'vim' },
+            },
             version = 'LuaJIT',
             path = runtime_path,
         },
         completion = {
             enable = true,
             callSnippet = 'Both',
-        },
-        diagnostics = {
-            enable = true,
-            globals = {
-                'vim',
-                'use',
-                'describe',
-                'it',
-                'assert',
-                'before_each',
-                'after_each',
-            },
+            keyboardSnippet = 'Both',
         },
         workspace = {
             library = {
@@ -37,7 +30,7 @@ local settings = {
             },
             checkThirdParty = false,
             maxPreload = 2000,
-            preloadFileSize = 5000,
+            preloadFileSize = 50000,
         },
 
         format = { enable = false },
@@ -55,10 +48,6 @@ M.setup = function(on_attach, capabilities)
         on_attach = on_attach,
         capabilities = capabilities,
         settings = settings,
-        flags = {
-            debounce_text_changes = 150,
-        },
-        single_file_support = true,
     })
 end
 
