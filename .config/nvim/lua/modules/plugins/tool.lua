27076c901c1f321800badb7f1512689a15e02020
David Estephanian <desteph@protonmail.com>
2023-03-10 16:59:21 -0800

    Removed trouble for now

--- .config/nvim/lua/modules/plugins/tool.lua
+++
@@ -1,9 +0,0 @@
-local tool = {}
-
-tool['folke/trouble.nvim'] = {
-    lazy = true,
-    cmd = { 'Trouble', 'TroubleToggle', 'TroubleRefresh' },
-    config = require('tool.trouble'),
-}
-
-return tool
