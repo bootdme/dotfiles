77f8ae853f9c1fbd89a6202238073ca4b965951f
David Estephanian <desteph@protonmail.com>
2023-03-12 12:34:25 -0700

    Added rust-tools plugin

---
+++ .config/nvim/lua/modules/plugins/lang.lua
@@ -0,0 +1,10 @@
+local lang = {}
+
+lang['simrat39/rust-tools.nvim'] = {
+    lazy = true,
+    ft = 'rust',
+    config = require('lang.rust-tools'),
+    dependencies = { 'nvim-lua/plenary.nvim' },
+}
+
+return lang
