2e9b6e4d7b30ed6faa939b5c30d21c527acc4e18
David Estephanian <desteph@protonmail.com>
2023-03-14 12:25:26 -0700

    Added telescope plugin

---
+++ .config/nvim/lua/modules/plugins/tool.lua
@@ -0,0 +1,16 @@
+local tool = {}
+
+tool['nvim-telescope/telescope.nvim'] = {
+    lazy = true,
+    cmd = 'Telescope',
+    config = require('tool.telescope'),
+    dependencies = {
+        { 'nvim-tree/nvim-web-devicons' },
+        { 'nvim-lua/plenary.nvim' },
+        { 'LinArcX/telescope-env.nvim' },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        { 'nvim-telescope/telescope-frecency.nvim', dependencies = { { 'kkharji/sqlite.lua' } } },
+    },
+}
+
+return tool
