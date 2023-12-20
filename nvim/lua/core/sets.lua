67d1c06530b2e6411559e8dea022fabb7b2aacb8
David Estephanian <desteph@protonmail.com>
2023-12-20 10:46:47 -0800

    Removed crates + updated plugins

--- nvim/lua/core/sets.lua
+++ nvim/lua/core/sets.lua
@@ -30,21 +30,6 @@ set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
 set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffer' })
 set('n', '<leader>fe', '<cmd>Telescope env<cr>', { desc = 'Find environment' })
 
--- c - crates
-set('n', '<leader>ct', '<cmd>lua require("crates").toggle()<cr>', { desc = 'Crates Toggle' })
-set('n', '<leader>cr', '<cmd>lua require("crates").reload()<cr>', { desc = 'Crates Reload' })
-set('n', '<leader>cv', '<cmd>lua require("crates").show_versions_popup()<cr>', { desc = 'Crates Versions' })
-set('n', '<leader>cf', '<cmd>lua require("crates").show_features_popup()<cr>', { desc = 'Crates Features' })
-set('n', '<leader>cd', '<cmd>lua require("crates").show_dependencies_popup()<cr>', { desc = 'Crates Dependencies' })
-set('n', '<leader>cu', '<cmd>lua require("crates").update_crate()<cr>', { desc = 'Crates Update' })
-set('v', '<leader>cu', '<cmd>lua require("crates").update_crates()<cr>', { desc = 'Crates Update(s)' })
-set('n', '<leader>ca', '<cmd>lua require("crates").update_all_crates()<cr>', { desc = 'Crates Update All' })
-set('n', '<leader>cU', '<cmd>lua require("crates").upgrade_crate()<cr>', { desc = 'Crates Upgrade' })
-set('v', '<leader>cU', '<cmd>lua require("crates").upgrade_crates()<cr>', { desc = 'Crates Upgrade(s)' })
-set('n', '<leader>ct', '<cmd>lua require("crates").upgrade_all_crates()<cr>', { desc = 'Crates Upgrade All' })
-set('n', '<leader>cD', '<cmd>lua require("crates").open_documentation()<cr>', { desc = 'Crates Documentation' })
-set('n', '<leader>cO', '<cmd>lua require("crates").open_crates_io()<cr>', { desc = 'Crates Open Crates' })
-
 -- m - markdown
 set('n', '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', { desc = 'Markdown Preview' })
 set('n', '<leader>ms', '<cmd>MarkdownPreviewStop<cr>', { desc = 'Markdown Stop' })
--- nvim/lua/modules/plugins/lang.lua
+++ nvim/lua/modules/plugins/lang.lua
@@ -14,11 +14,4 @@ lang['simrat39/rust-tools.nvim'] = {
     dependencies = { 'nvim-lua/plenary.nvim' },
 }
 
-lang['Saecki/crates.nvim'] = {
-    lazy = true,
-    event = 'BufReadPost Cargo.toml',
-    config = require('lang.crates'),
-    dependencies = { 'nvim-lua/plenary.nvim' },
-}
-
 return lang
